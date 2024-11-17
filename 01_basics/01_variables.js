5c5785b4459a168e053044a968daddec5cd436a7
Bhaloo-bit <b604363@gmail.com>
2024-11-17 05:15:47 +0000

    conversion of datatype

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const Account_id=12344
+let accountEmail="bipul@gmail.com"
+var accountPassword ="1234"
+accountCity = "jaipur"
+let accountState;
+
+// Account_id =2 not allowed
+accountEmail="sbhajk@gmail.com" 
+accountPassowrd ="234234"
+accountCity="delhi"
+// console.log(Account_id);
+console.table([accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+ let score = null
+
+ console.log(typeof score);
+
+ let valueInNumber =Number(score)
+ console.log(typeof valueInNumber);
+
+//  "33" => 33
+//  "33abc" => Nan
+// true  => 1; false => 0
+
+let isloggedin = "bhaloo"
+
+ let isboolenloggedin = Boolean( isloggedin)
+ console.log(isboolenloggedin);
+
+
+// 1 => ture;  0=> false
+// "" => false 
+// "bhaloo" => true 
+
+let someNumber= 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof (stringNumber))
