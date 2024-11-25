442182d48bb4e473f683db2314bf79b2ea19d425
Bhaloo-bit <b604363@gmail.com>
2024-11-25 12:43:41 +0000

    done

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -21,15 +21,33 @@ const obj2 = {3:"c",4:"d"}
 
 // const obj3 = Object.assign({}, obj1, obj2)
 
-const obj3 = {...obj1, ...obj2}
-console.log(obj3)
-console.log(tinderuser);
+// const obj3 = {...obj1, ...obj2}
+// console.log(obj3)
+// console.log(tinderuser);
 
-console.log(Object.keys(tinderuser));
-console.log(Object.values(tinderuser));
-console.log(Object.entries(tinderuser));
+// console.log(Object.keys(tinderuser));
+// console.log(Object.values(tinderuser));
+// console.log(Object.entries(tinderuser));
 
-console.log(tinderUser.hasOWnproperty('isLoggedIn'))
+// console.log(tinderUser.hasOWnproperty('isLoggedIn'))
 
 
+const course ={
+    coursename: "js in hindi",
+    price :"999",
+    courseInstructor :"hitesh" 
 
+}
+
+// course.courseInstructor
+// destruture of objects
+
+const {courseInstructor: cosint}= course
+console.log(cosint);
+
+// {
+//     "name": "hitesh",
+//     "coursename ": "js in hindo",
+//     "price": "free"
+
+// }
