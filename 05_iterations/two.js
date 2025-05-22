7b095050b69c515d042a56080492db2dcea11974
Bhaloo-bit <b604363@gmail.com>
2025-05-22 06:56:52 +0000

    whie loop comepleted

---
+++ 05_iterations/two.js
@@ -0,0 +1,16 @@
+// let index = 0
+// while (index<=10) {
+//     console.log(`Value of index is ${index}`)
+//     index = index +2
+//     continue
+// }
+
+let Arry =["spiderman" , "superman ", "aquaman"]
+let arr=0
+while (arr<Arry.length) {
+    console.log(`My favorite Super hores are ${Arry[arr]}`)
+    arr =arr +1
+}
+do {
+    
+} while (condition);
